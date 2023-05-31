30c2cba6a82551c9802b482309c2bfe8dce01c21
Scott Barnard <scott@ausocean.org>
2023-05-31 17:34:32 +1000

    Refactor API tests

---
+++ api/capturesource_test.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/ausocean/openfish/api/api"
+	"github.com/ausocean/openfish/api/ds_client"
+	"github.com/ausocean/openfish/api/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestCreateCaptureSource(t *testing.T) {
+
+	testcases := []struct {
+		reqBody   string
+		resStatus int
+	}{
+		// TODO: Test cases with missing fields cause the server to crash.
+		//       Add test case for this and fix issue.
+		{
+			resStatus: fiber.StatusOK,
+			reqBody: `{
+				"name": "Ochre Point",
+				"location": "-35.221389,138.46333333",
+				"camera_hardware": "rpi cam v2 - wide angle lens",
+				"site_id": 10192840284
+			}`,
+		},
+		{
+			resStatus: fiber.StatusOK,
+			reqBody: `{
+				"name": "Ochre Point",
+				"location": "-35.221389,138.46333333",
+				"camera_hardware": "rpi cam v2 - wide angle lens"
+			}`,
+		},
+		{
+			resStatus: fiber.StatusOK,
+			reqBody: `{
+				"name": "Ochre Point",
+				"location": "-35.221389,138.46333333"
+			}`,
+		},
+		{
+			resStatus: fiber.StatusBadRequest,
+			reqBody: `{
+				"name": true,
+				"location": [1,2,3],
+				"camera_hardware": 123456,
+				"site_id": false,
+				"extra": "fields"
+			}`,
+		},
+		{
+			resStatus: fiber.StatusBadRequest,
+			reqBody:   "something that is not json",
+		},
+		{
+			resStatus: fiber.StatusBadRequest,
+			reqBody:   "",
+		},
+	}
+
+	// Initialize app.
+	ds_client.Init(false)
+	app := fiber.New()
+	RegisterAPIRoutes(app)
+
+	for i, testcase := range testcases {
+		// Send test request.
+		req := httptest.NewRequest("POST", "/api/v1/capturesources", strings.NewReader(testcase.reqBody))
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req, -1)
+
+		// Check request, response.
+		if err != nil {
+			t.Errorf("[test %d]  %s", i, err.Error())
+		}
+
+		if resp.StatusCode != testcase.resStatus {
+			t.Errorf("[test %d]  expected HTTP code %d, got %d", i, testcase.resStatus, resp.StatusCode)
+		}
+	}
+}
+
+func TestGetCaptureSourcesWithLimitAndOffset(t *testing.T) {
+
+	// Initialize app.
+	ds_client.Init(false)
+	app := fiber.New()
+	RegisterAPIRoutes(app)
+
+	// Populate datastore with test data.
+	for _, body := range []string{
+		`{
+			"name": "Ochre Point",
+			"location": "-35.221389,138.46333333",
+			"camera_hardware": "rpi cam v2 - wide angle lens",
+			"site_id": 10192840284
+		}`,
+		`{
+			"name": "Carrickalinga",
+			"location": "-35.4270000,138.3148330",
+			"camera_hardware": "rpi cam v2"
+		}`,
+		`{
+			"name": "Windara",
+			"location": "-35.221389,138.46333333",
+			"camera_hardware": "rpi cam v2 - wide angle lens",
+			"site_id": 12345678910
+		}`,
+	} {
+		// Send test request.
+		req := httptest.NewRequest("POST", "/api/v1/capturesources", strings.NewReader(body))
+		req.Header.Set("Content-Type", "application/json")
+		app.Test(req, -1)
+	}
+
+	testcases := []struct {
+		limit  int
+		offset int
+	}{
+		{
+			limit: 0, offset: 0,
+		},
+		{
+			limit: 1, offset: 0,
+		},
+		{
+			limit: 2, offset: 0,
+		},
+		{
+			limit: 1, offset: 1,
+		},
+		{
+			limit: 2, offset: 1,
+		},
+	}
+
+	for i, testcase := range testcases {
+		// Send request
+		url := fmt.Sprintf("/api/v1/capturesources?limit=%d&offset=%d", testcase.limit, testcase.offset)
+		req := httptest.NewRequest("GET", url, nil)
+		resp, err := app.Test(req)
+		if err != nil {
+			t.Errorf("[test %d]  %s", i, err.Error())
+		}
+
+		// Check response.
+		if resp.StatusCode != fiber.StatusOK {
+			t.Errorf("[test %d]  expected HTTP code 200, got %d", i, resp.StatusCode)
+		}
+
+		// Unmarshal response.
+		respBody, _ := ioutil.ReadAll(resp.Body)
+		var result api.Result[handlers.CaptureSourceResult]
+		if json.Unmarshal(respBody, &result) != nil {
+			t.Errorf("[test %d]  invalid JSON format %s", i, string(respBody))
+		}
+
+		// Check response has correct limit and offset.
+		if len(result.Results) != testcase.limit {
+			t.Errorf("[test %d]  expected %d result, got %d results", i, testcase.limit, len(result.Results))
+		}
+
+		if result.Limit != testcase.limit {
+			t.Errorf("[test %d]  expected limit to be equal to %d, got limit = %d", i, testcase.limit, result.Limit)
+		}
+
+		if result.Offset != testcase.offset {
+			t.Errorf("[test %d]  expected offset to be equal to %d, got offset = %d", i, testcase.offset, result.Offset)
+		}
+	}
+}
---
+++ api/videostream_test.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/ausocean/openfish/api/ds_client"
+	"github.com/gofiber/fiber/v2"
+)
+
+func setup(t *testing.T) (*fiber.App, int64) {
+	// Initialize app.
+	ds_client.Init(false)
+	app := fiber.New()
+	RegisterAPIRoutes(app)
+
+	// Populate datastore with capturesource for testing.
+	// Send test request.
+	body := `{
+		"name": "Ochre Point",
+		"location": "-35.221389,138.46333333",
+		"camera_hardware": "rpi cam v2 - wide angle lens",
+		"site_id": 10192840284
+	}`
+	req := httptest.NewRequest("POST", "/api/v1/capturesources", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	res, err := app.Test(req, -1)
+	if err != nil {
+		t.Fatalf("test setup failed: %s", err.Error())
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("test setup failed: status code = %d", res.StatusCode)
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("test setup failed: %s", err.Error())
+	}
+
+	csID, err := extractID(resBody)
+	if err != nil {
+		t.Fatalf("test setup failed: %s", err.Error())
+	}
+
+	return app, csID
+}
+
+func teardown() {
+	// TODO: remove testing capturesource.
+}
+
+// extractID extracts the ID from the response.
+func extractID(body []byte) (int64, error) {
+	var resData struct {
+		ID int64
+	}
+
+	err := json.Unmarshal(body, &resData)
+	if err != nil {
+		return 0, err
+	}
+
+	return resData.ID, nil
+}
+
+// Checks that JSON has the expected format.
+func bodyMatchesType[T any](body []byte) bool {
+	var format T
+	return json.Unmarshal(body, &format) == nil
+}
+
+// TestCreateVideoStream checks that the API for creating a video stream returns a 200 (ok) response
+// when a valid request is made, and a 400 (bad request) response otherwise.
+func TestCreateVideoStream(t *testing.T) {
+	app, csID := setup(t)
+	defer teardown()
+
+	testcases := []struct {
+		reqBody       string // Test case data.
+		resStatusCode int    // Expected response: HTTP status code.
+		resIsSuccess  bool   // Expected response: Has body with format {"id": <number>} if successful, else { "message": <string> } if not.
+	}{
+		{
+			// Valid requests.
+			reqBody: `{
+				"stream_url": "https://www.youtube.com/watch?v=abcdefghijk",
+				"capturesource": %d,
+				"startTime": "2023-06-07T08:00:00.00Z",
+				"endTime": "2023-06-07T16:30:00.00Z"
+			}`,
+			resStatusCode: fiber.StatusOK,
+			resIsSuccess:  true,
+		},
+		{
+			reqBody: `{
+				"stream_url": "https://www.youtube.com/watch?v=1234567890A",
+				"capturesource": %d,
+				"startTime": "2023-05-01T20:00:00.00Z",
+				"endTime": "2023-05-02T06:15:00.00Z"
+			}`,
+			resStatusCode: fiber.StatusOK,
+			resIsSuccess:  true,
+		},
+		// Should be a bad request because end time is before start time.
+		{
+			reqBody: `{
+				"stream_url": "https://www.youtube.com/watch?v=abcdefghijk",
+				"capturesource": %d,
+				"startTime": "2023-05-01T20:00:00.00Z",
+				"endTime": "2022-05-01T20:00:00.00Z"
+			}`,
+			resStatusCode: fiber.StatusOK,
+			resIsSuccess:  true,
+		},
+		// Should be bad requests because missing required fields.
+		{
+			reqBody: `{
+				"stream_url": "https://www.youtube.com/watch?v=abcdefghijk",
+				"startTime": "2023-06-07T08:00:00.00Z",
+				"endTime": "2023-06-07T16:30:00.00Z"
+			}`,
+			resStatusCode: fiber.StatusBadRequest,
+			resIsSuccess:  false,
+		},
+		{
+			reqBody: `{
+				"stream_url": "https://www.youtube.com/watch?v=abcdefghijk",
+				"capturesource": %d,
+				"endTime": "2023-06-07T16:30:00.00Z"
+			}`,
+			resStatusCode: fiber.StatusBadRequest,
+			resIsSuccess:  false,
+		},
+		{
+			reqBody: `{
+				"stream_url": "https://www.youtube.com/watch?v=abcdefghijk",
+				"capturesource": %d,
+				"startTime": "2023-06-07T08:00:00.00Z"
+			}`,
+			resStatusCode: fiber.StatusBadRequest,
+			resIsSuccess:  false,
+		},
+		// Should be bad requests because they are invalid JSON.
+		{
+			reqBody:       "something that is not json",
+			resStatusCode: fiber.StatusBadRequest,
+			resIsSuccess:  false,
+		},
+		{
+			reqBody:       "",
+			resStatusCode: fiber.StatusBadRequest,
+			resIsSuccess:  false,
+		},
+	}
+
+	for i, testcase := range testcases {
+		// Insert data into test cases.
+		reqBody := fmt.Sprintf(testcase.reqBody, csID)
+
+		// Send test request.
+		req := httptest.NewRequest("POST", "/api/v1/videostreams", strings.NewReader(reqBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		res, err := app.Test(req, -1)
+		if err != nil {
+			t.Errorf("[test %d] app.Test failed with error: %s", i, err)
+		}
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Errorf("[test %d]  io.ReadAll failed with error: %s", i, err)
+		}
+
+		// Check response status code matches expected.
+		if res.StatusCode != testcase.resStatusCode {
+			t.Errorf("[test %d]  expected HTTP code %d, got %d", i, testcase.resStatusCode, res.StatusCode)
+		}
+
+		// Check response body matches expected.
+		if testcase.resIsSuccess && !bodyMatchesType[struct{ ID uint64 }](body) {
+			t.Errorf(`[test %d]  expected response body to have format { "id": <int> }, got %s`, i, string(body))
+		}
+		if !testcase.resIsSuccess && !bodyMatchesType[struct{ Message string }](body) {
+			t.Errorf(`[test %d]  expected response body to have format { "message": <string> }, got %s`, i, string(body))
+		}
+
+		// Clean up after ourselves by removing the video stream.
+		if testcase.resIsSuccess {
+			id, _ := extractID(body)
+			req := httptest.NewRequest("DELETE", fmt.Sprintf("/api/v1/videostreams/%d", id), nil)
+			app.Test(req, -1)
+		}
+	}
+}
+
+func TestDeleteVideoStream(t *testing.T) {
+	app, csID := setup(t)
+	defer teardown()
+
+	t.Run("it should return 200 for valid ID", func(t *testing.T) {
+
+		// Create a video stream to delete later in test
+		createReqBody := `{
+			"stream_url": "https://www.youtube.com/watch?v=abcdefghijk",
+			"capturesource": %d,
+			"startTime": "2023-05-01T20:00:00.00Z",
+			"endTime": "2022-05-01T20:00:00.00Z"
+		}`
+		createReqBody = fmt.Sprintf(createReqBody, csID)
+
+		createReq := httptest.NewRequest("POST", "/api/v1/videostreams", strings.NewReader(createReqBody))
+		createReq.Header.Set("Content-Type", "application/json")
+		createRes, err := app.Test(createReq, -1)
+		if err != nil {
+			t.Logf("app.Test failed with error: %s", err)
+			t.Fail()
+		}
+		createResBody, _ := io.ReadAll(createRes.Body)
+		id, _ := extractID(createResBody)
+
+		// Send delete request.
+		req := httptest.NewRequest("DELETE", fmt.Sprintf("/api/v1/videostreams/%d", id), nil)
+		res, err := app.Test(req, -1)
+
+		if err != nil {
+			t.Errorf("app.Test failed with error: %s", err)
+		}
+
+		// Check response status code matches expected.
+		if res.StatusCode != 200 {
+			t.Errorf("expected HTTP code 200, got %d", res.StatusCode)
+		}
+
+	})
+
+	t.Run("it should return an error for nonexistent IDs", func(t *testing.T) {
+		// Send delete request.
+		req := httptest.NewRequest("DELETE", fmt.Sprintf("/api/v1/videostreams/%d", 1234), nil)
+		res, err := app.Test(req, -1)
+
+		if err != nil {
+			t.Errorf("app.Test failed with error: %s", err)
+		}
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Errorf("io.ReadAll failed with error: %s", err)
+		}
+
+		// Check response status code matches expected.
+		if res.StatusCode != 400 {
+			t.Errorf("expected HTTP code 400, got %d", res.StatusCode)
+		}
+
+		// Check response body matches expected.
+		if !bodyMatchesType[struct{ Message string }](body) {
+			t.Errorf(`expected response body to have format { "message": <string> }, got %s`, string(body))
+		}
+	})
+}
