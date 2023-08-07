ecfc2d4fa3d0ae0b38352ce0c8e0ce61a88ae104
Alan Noble <anoble@gmail.com>
2023-08-07 17:12:54 +0930

    Improved some comments.

--- datastore/cache.go
+++ datastore/cache.go
@@ -38,7 +38,7 @@ import (
 	"sync"
 )
 
-// Type Cache defines the (optional) caching interface used by Entity.
+// Cache defines the (optional) caching interface used by Entity.
 type Cache interface {
 	Set(key *Key, value Entity)   // Set adds or updates a value to the cache.
 	Get(key *Key) (Entity, error) // Get retrieves a value from the cache, or returns ErrcacheMiss.
@@ -46,7 +46,8 @@ type Cache interface {
 	Reset()                       // Reset resets (clears) the cache.
 }
 
-// EntityCache represents a cache for holding datastore entities indexed by key.
+// EntityCache, which implements Cache, represents a cache for holding
+// datastore entities indexed by key.
 type EntityCache struct {
 	data  map[Key]Entity
 	mutex sync.RWMutex
