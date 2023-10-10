70b8e56dca53b4355328419dda2f6b8caa911ba0
Scott Barnard <scott@ausocean.org>
2023-10-11 10:50:15 +1100

    Add API types

---
+++ openfish-webapp/src/api.types.ts
@@ -0,0 +1,33 @@
+export interface Annotation {
+    id: number
+    videostreamId: number
+    timespan: Timespan
+    boundingBox?: BoundingBox
+    observer: string
+    observation: Observation
+  }
+  
+  export interface Observation {
+    [key: string]: string
+  }
+  
+  export interface Timespan {
+    start: string
+    end: string
+  }
+  
+  export interface BoundingBox {
+    x1: number
+    y1: number
+    x2: number
+    y2: number
+  }
+  
+  export interface VideoStream {
+    id: number
+    stream_url: string
+    capturesource: string
+    startTime: string
+    endTime: string
+  }
+  
