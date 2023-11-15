79f56530b3ce2917d72b3dda22bb107a6f0523af
Scott Barnard <scott@ausocean.org>
2023-11-15 11:50:30 +1100

    Replace rome formatter/linter with biome
    Rome was discontinued and forked, now called Biome.

--- openfish-webapp/README.md
+++ openfish-webapp/README.md
@@ -50,4 +50,4 @@ Name | Description | Purpose
 [Lit](https://lit.dev/) | A library used to create [custom elements / web components](https://developer.mozilla.org/en-US/docs/Web/API/Web_Components/Using_custom_elements) | Webcomponents let us break a site down into smaller reusable sections, while can still be used with vanilla html, css & js, without needing a framework.
 [Vite](https://vitejs.dev/) | Development server / bundler | During development changes made can be reflected in the browser instantly because of its hot-module reloading feature.
 [Typescript](https://www.typescriptlang.org/) | Static type checking for Javascript | Typescript provides type information in code editors / IDEs and catches mistakes.
-[Rome](https://rome.tools/) | Code formatter and linter | Helps us keep code style consistent and catches mistakes.
+[Biome](https://biomejs.dev/) | Code formatter and linter | Helps us keep code style consistent and catches mistakes.
--- openfish-webapp/src/api.types.ts
+++ openfish-webapp/src/api.types.ts
@@ -1,33 +1,32 @@
 export interface Annotation {
-    id: number
-    videostreamId: number
-    timespan: Timespan
-    boundingBox?: BoundingBox
-    observer: string
-    observation: Observation
-  }
-  
-  export interface Observation {
-    [key: string]: string
-  }
-  
-  export interface Timespan {
-    start: string
-    end: string
-  }
-  
-  export interface BoundingBox {
-    x1: number
-    y1: number
-    x2: number
-    y2: number
-  }
-  
-  export interface VideoStream {
-    id: number
-    stream_url: string
-    capturesource: string
-    startTime: string
-    endTime: string
-  }
-  
+  id: number
+  videostreamId: number
+  timespan: Timespan
+  boundingBox?: BoundingBox
+  observer: string
+  observation: Observation
+}
+
+export interface Observation {
+  [key: string]: string
+}
+
+export interface Timespan {
+  start: string
+  end: string
+}
+
+export interface BoundingBox {
+  x1: number
+  y1: number
+  x2: number
+  y2: number
+}
+
+export interface VideoStream {
+  id: number
+  stream_url: string
+  capturesource: string
+  startTime: string
+  endTime: string
+}
