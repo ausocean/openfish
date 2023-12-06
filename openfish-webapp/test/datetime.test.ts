42782b2673aeb482de7a053d5efded4c41284efb
Scott Barnard <scott@ausocean.org>
2023-12-06 12:19:44 +1100

    Setup vitest for unit testing of typescript

---
+++ openfish-webapp/test/datetime.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+import { datetimeDifference } from '../src/datetime'
+
+test('datetimeDifference calculates time correctly', () => {
+  const a = '2023-12-06T12:17:00.000Z'
+  const b = '2023-12-06T12:19:00.000Z'
+  const diff = datetimeDifference(b, a)
+  expect(diff).toBe(120)
+})
