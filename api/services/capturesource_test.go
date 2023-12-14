17034a871491980157374bf56b373cd4e58603b5
Scott Barnard <scott@ausocean.org>
2023-12-14 22:23:30 +1100

    Write unit tests for services package

---
+++ api/services/capturesource_test.go
@@ -0,0 +1,223 @@
+/*
+AUTHORS
+  Scott Barnard <scott@ausocean.org>
+
+LICENSE
+  Copyright (c) 2023, The OpenFish Contributors.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of The Australian Ocean Lab Ltd. ("AusOcean")
+     nor the names of its contributors may be used to endorse or promote
+     products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package services_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ausocean/openfish/api/ds_client"
+	"github.com/ausocean/openfish/api/services"
+)
+
+func setup() {
+	ds_client.Init(true)
+
+	// Create directories if they do not exist.
+	_ = os.Mkdir("store/openfish/CaptureSource", os.ModePerm)
+	_ = os.Mkdir("store/openfish/VideoStream", os.ModePerm)
+	_ = os.Mkdir("store/openfish/Annotation", os.ModePerm)
+
+}
+
+func TestCreateCaptureSource(t *testing.T) {
+	setup()
+
+	// Create a new capture source entity.
+	_, err := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	if err != nil {
+		t.Errorf("Could not create capture source entity %s", err)
+	}
+}
+
+func TestCaptureSourceExists(t *testing.T) {
+	setup()
+
+	// Create a new capture source entity.
+	id, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+
+	// Check if the capture source exists.
+	if !services.CaptureSourceExists(int64(id)) {
+		t.Errorf("Expected capture source to exist")
+	}
+}
+
+func TestCaptureSourceExistsForNonexistentEntity(t *testing.T) {
+	setup()
+
+	// Check if the capture source exists.
+	// We expect it to return false.
+	if services.CaptureSourceExists(int64(123456789)) {
+		t.Errorf("Did not expect capture source to exist")
+	}
+}
+
+func TestGetCaptureSourceByID(t *testing.T) {
+	setup()
+
+	// Define test cases.
+	testCases := []struct {
+		name           string
+		lat            float64
+		long           float64
+		cameraHardware string
+	}{
+		{"Stony Point camera 1", 0.0, 0.0, "RPI camera"},
+		{"CuttleCam", -100.0, 100.0, "wide angle camera"},
+	}
+
+	for _, tc := range testCases {
+		// Create capture source entities for each test case.
+		id, _ := services.CreateCaptureSource(tc.name, tc.lat, tc.long, tc.cameraHardware, nil)
+
+		// Check if the capture sources can be fetched and is the same.
+		captureSource, err := services.GetCaptureSourceByID(int64(id))
+		if err != nil {
+			t.Errorf("Could not get capture source entity %s", err)
+		}
+		if captureSource.Name != tc.name || captureSource.Location.Lat != tc.lat || captureSource.Location.Lng != tc.long || captureSource.CameraHardware != tc.cameraHardware {
+			t.Errorf("Capture source entity does not match created entity")
+		}
+	}
+}
+
+func TestGetCaptureSourceByIDForNonexistentEntity(t *testing.T) {
+	setup()
+
+	captureSource, err := services.GetCaptureSourceByID(int64(123456789))
+	if captureSource != nil && err == nil {
+		t.Errorf("GetCaptureSourceByID returned non-existing entity %s", err)
+	}
+}
+
+// TODO: Write tests for GetCaptureSources. Test limit, offset and filtering.
+
+func TestUpdateCaptureSource(t *testing.T) {
+	setup()
+
+	// Create a new capture source entity.
+	id, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+
+	// Update the name.
+	name := "new name"
+	err := services.UpdateCaptureSource(int64(id), &name, nil, nil, nil, nil)
+	if err != nil {
+		t.Errorf("Could not update capture source entity %s", err)
+	}
+
+	captureSource, _ := services.GetCaptureSourceByID(int64(id))
+	if captureSource.Name != name {
+		t.Errorf("Name did not update, expected %s, actual %s", name, captureSource.Name)
+	}
+
+	// Update latitude and longitude.
+	lat, long := -37.0, 145.0
+	err = services.UpdateCaptureSource(int64(id), nil, &lat, &long, nil, nil)
+	if err != nil {
+		t.Errorf("Could not update capture source entity %s", err)
+	}
+
+	captureSource, _ = services.GetCaptureSourceByID(int64(id))
+	if captureSource.Location.Lat != lat || captureSource.Location.Lng != long {
+		t.Errorf("Location did not update, expected %f %f, actual %f %f", lat, long, captureSource.Location.Lat, captureSource.Location.Lng)
+	}
+
+	// Update cameraHardware.
+	cameraHardware := "RPI wide angle camera"
+	err = services.UpdateCaptureSource(int64(id), nil, nil, nil, &cameraHardware, nil)
+	if err != nil {
+		t.Errorf("Could not update capture source entity %s", err)
+	}
+
+	captureSource, _ = services.GetCaptureSourceByID(int64(id))
+	if captureSource.CameraHardware != cameraHardware {
+		t.Errorf("CameraHardware did not update, expected %s, actual %s", cameraHardware, captureSource.CameraHardware)
+	}
+
+	// TODO: test updating siteID.
+}
+
+func TestUpdateCaptureSourceForNonExistentEntity(t *testing.T) {
+	setup()
+
+	err := services.UpdateCaptureSource(int64(123456789), nil, nil, nil, nil, nil)
+	if err == nil {
+		t.Errorf("Did not receive expected error when updating non-existent capture source")
+	}
+}
+
+func TestDeleteCaptureSource(t *testing.T) {
+	setup()
+
+	// Create a new capture source entity.
+	id, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+
+	// Delete the capture source entity.
+	err := services.DeleteCaptureSource(int64(id))
+	if err != nil {
+		t.Errorf("Could not delete capture source entity %d: %s", id, err)
+	}
+
+	// Check if the capture source exists.
+	if services.CaptureSourceExists(int64(id)) {
+		t.Errorf("Capture source entity exists after delete")
+	}
+}
+
+func TestDeleteCaptureSourceForNonexistentEntity(t *testing.T) {
+	setup()
+
+	err := services.DeleteCaptureSource(int64(123456789))
+	if err == nil {
+		t.Errorf("Did not receive expected error when deleting non-existent capture source")
+	}
+}
+
+func TestDeleteCaptureSourceWithAssociatedVideoStreams(t *testing.T) {
+	// TODO: Run test in CI when issue is fixed.
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	setup()
+
+	// Create a new capture source entity and a video stream that references it.
+	id, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	services.CreateVideoStream("http://youtube.com/watch?v=abc123", int64(id), _8am, &_4pm)
+
+	err := services.DeleteCaptureSource(int64(id))
+	if err == nil {
+		t.Errorf("Did not receive expected error when deleting capture source with associated video stream")
+	}
+}
---
+++ api/services/videostream_test.go
@@ -0,0 +1,216 @@
+/*
+AUTHORS
+  Scott Barnard <scott@ausocean.org>
+
+LICENSE
+  Copyright (c) 2023, The OpenFish Contributors.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of The Australian Ocean Lab Ltd. ("AusOcean")
+     nor the names of its contributors may be used to endorse or promote
+     products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package services_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ausocean/openfish/api/model"
+	"github.com/ausocean/openfish/api/services"
+)
+
+// Constants.
+var _8am = time.Date(2023, time.January, 1, 8, 0, 0, 0, time.UTC)
+var _9am = time.Date(2023, time.January, 1, 9, 0, 0, 0, time.UTC)
+var _4pm = time.Date(2023, time.January, 1, 16, 0, 0, 0, time.UTC)
+var _5pm = time.Date(2023, time.January, 1, 17, 0, 0, 0, time.UTC)
+
+func TestCreateVideoStream(t *testing.T) {
+	setup()
+
+	// Create a new video stream entity.
+	cs, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	_, err := services.CreateVideoStream("http://youtube.com/watch?v=abc123", int64(cs), _8am, &_4pm)
+	if err != nil {
+		t.Errorf("Could not create video stream entity %s", err)
+	}
+}
+
+func TestVideoStreamExists(t *testing.T) {
+	setup()
+
+	// Create a new video stream entity.
+	cs, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	id, _ := services.CreateVideoStream("http://youtube.com/watch?v=abc123", int64(cs), _8am, &_4pm)
+
+	// Check if the video stream exists.
+	if !services.VideoStreamExists(int64(id)) {
+		t.Errorf("Expected video stream to exist")
+	}
+}
+
+func TestVideoStreamExistsForNonexistentEntity(t *testing.T) {
+	setup()
+
+	// Check if the video stream exists.
+	// We expect it to return false.
+	if services.VideoStreamExists(int64(123456789)) {
+		t.Errorf("Did not expect video stream to exist")
+	}
+}
+
+func TestGetVideoStreamByID(t *testing.T) {
+	setup()
+
+	cs, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	id, _ := services.CreateVideoStream("http://youtube.com/watch?v=abc123", int64(cs), _8am, &_4pm)
+
+	videoStream, err := services.GetVideoStreamByID(int64(id))
+	if err != nil {
+		t.Errorf("Could not get video stream entity %s", err)
+	}
+	if videoStream.CaptureSource != int64(cs) && videoStream.StartTime != _8am && *videoStream.EndTime != _4pm && videoStream.StreamUrl != "http://youtube.com/watch?v=abc123" {
+		t.Errorf("Video stream entity does not match created entity")
+	}
+}
+
+func TestGetVideoStreamByIDForNonexistentEntity(t *testing.T) {
+	setup()
+
+	videoStream, err := services.GetVideoStreamByID(int64(123456789))
+	if videoStream != nil && err == nil {
+		t.Errorf("GetVideoStreamByID returned non-existing entity %s", err)
+	}
+}
+
+// TODO: Write tests for GetVideoStreams. Test limit, offset and filtering.
+
+func TestUpdateVideoStream(t *testing.T) {
+	setup()
+	// Create a new video stream entity.
+	cs, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	id, _ := services.CreateVideoStream("http://youtube.com/watch?v=abc123", int64(cs), _8am, &_4pm)
+
+	// Update the url.
+	url := "http://youtube.com/watch?v=xyz789"
+	err := services.UpdateVideoStream(int64(id), &url, nil, nil, nil)
+	if err != nil {
+		t.Errorf("Could not update video stream entity %s", err)
+	}
+
+	videoStream, _ := services.GetVideoStreamByID(int64(id))
+	if videoStream.StreamUrl != url {
+		t.Errorf("URL did not update, expected %s, actual %s", url, videoStream.StreamUrl)
+	}
+
+	// Update the capture source.
+	cs2, _ := services.CreateCaptureSource("Stony Point camera 2", 0.0, 0.0, "RPI camera", nil)
+	csnew := int64(cs2)
+	err = services.UpdateVideoStream(int64(id), nil, &csnew, nil, nil)
+	if err != nil {
+		t.Errorf("Could not update video stream entity %s", err)
+	}
+
+	videoStream, _ = services.GetVideoStreamByID(int64(id))
+	if videoStream.CaptureSource != csnew {
+		t.Errorf("Capture source did not update, expected %d, actual %d", csnew, videoStream.CaptureSource)
+	}
+
+	// Update stream times.
+	err = services.UpdateVideoStream(int64(id), nil, nil, &_9am, &_5pm)
+	if err != nil {
+		t.Errorf("Could not update video stream entity %s", err)
+	}
+
+	videoStream, _ = services.GetVideoStreamByID(int64(id))
+	if !videoStream.StartTime.Equal(_9am) || !videoStream.EndTime.Equal(_5pm) {
+		t.Errorf("Start / end times did not update, expected %s %s, actual %s %s",
+			_9am.Format(time.RFC3339),
+			_5pm.Format(time.RFC3339),
+			videoStream.StartTime.Format(time.RFC3339),
+			videoStream.EndTime.Format(time.RFC3339),
+		)
+	}
+
+}
+
+func TestUpdateVideoStreamForNonExistentEntity(t *testing.T) {
+	setup()
+
+	err := services.UpdateVideoStream(int64(123456789), nil, nil, nil, nil)
+	if err == nil {
+		t.Errorf("Did not receive expected error when updating non-existent video stream")
+	}
+}
+
+func TestDeleteVideoStream(t *testing.T) {
+	setup()
+
+	// Create a new video stream entity.
+	cs, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	id, _ := services.CreateVideoStream("http://youtube.com/watch?v=abc123", int64(cs), _8am, &_4pm)
+
+	// Delete the video stream entity.
+	err := services.DeleteVideoStream(int64(id))
+	if err != nil {
+		t.Errorf("Could not delete video stream entity %d: %s", id, err)
+	}
+
+	// Check if the video stream exists.
+	if services.VideoStreamExists(int64(id)) {
+		t.Errorf("Video stream entity exists after delete")
+	}
+}
+
+func TestDeleteVideoStreamForNonexistentEntity(t *testing.T) {
+	setup()
+
+	err := services.DeleteVideoStream(int64(123456789))
+	if err == nil {
+		t.Errorf("Did not receive expected error when deleting non-existent video stream")
+	}
+}
+
+func TestDeleteVideoStreamWithAssociatedAnnotations(t *testing.T) {
+	// TODO: Run test in CI when issue is fixed.
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	setup()
+
+	// Create a new video stream entity and an annotation that references it.
+	cs, _ := services.CreateCaptureSource("Stony Point camera 1", 0.0, 0.0, "RPI camera", nil)
+	id, _ := services.CreateVideoStream("http://youtube.com/watch?v=abc123", int64(cs), _8am, &_4pm)
+	services.CreateAnnotation(id,
+		model.TimeSpan{Start: _8am, End: _9am},
+		nil, "scott@ausocean.org",
+		map[string]string{"species": "Sepia Apama"})
+
+	err := services.DeleteVideoStream(int64(id))
+	if err == nil {
+		t.Errorf("Did not receive expected error when deleting video stream with associated annotation")
+	}
+}
