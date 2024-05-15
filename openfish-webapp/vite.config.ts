ab3022554468ec7ec51a2691d0c88f54598b7064
Scott Barnard <scott@ausocean.org>
2024-05-15 16:41:30 +1000

    Use glob to determine vite build inputs

--- openfish-webapp/vite.config.ts
+++ openfish-webapp/vite.config.ts
@@ -1,17 +1,22 @@
 // vite.config.js
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import { vite as vidstack } from 'vidstack/plugins'
+import { globSync } from 'glob'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const input = Object.fromEntries(
+  globSync('*.html').map((file) => [
+    file.slice(0, file.length - path.extname(file).length),
+    fileURLToPath(new URL(file, import.meta.url)),
+  ])
+)
 
 export default defineConfig({
   plugins: [vidstack()],
   build: {
     rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'index.html'),
-        watch: resolve(__dirname, 'watch.html'),
-        streams: resolve(__dirname, 'streams.html'),
-      },
+      input,
     },
   },
 })
