a2d9c233ffe50c3139e6a1bd7a267b8e7bb3ab14
Scott Barnard <scott@ausocean.org>
2024-10-05 16:51:42 +1000

    Add support for accessing logged in user through global context
    This is so we can show user interface items conditionally based upon user role. The user does not have to be passed as props, it is available to its children via the user context.

--- openfish-webapp/src/webcomponents/site-nav.ts
+++ openfish-webapp/src/webcomponents/site-nav.ts
@@ -1,15 +1,39 @@
 import { LitElement, css, html, unsafeCSS } from 'lit'
 import { customElement } from 'lit/decorators.js'
 import resetcss from '../styles/reset.css?raw'
+import type { User } from '../api/user'
+import { consume } from '@lit/context'
+import { userContext } from '../utils/context'
 
 @customElement('site-nav')
 export class SiteNav extends LitElement {
+  @consume({ context: userContext, subscribe: true })
+  user: User | null = null
+
   render() {
+    let user = html``
+    if (this.user !== null) {
+      user = html`
+      <li>
+        <span class="tag">${this.user?.role}</span>
+        ${this.user?.email} 
+      </li>
+      `
+    }
+
     return html`
     <h1><a href="/">OpenFish</a></h1>
     <menu>
         <li><a href="/streams.html">View streams</a></li>
-        <li><a href="/capturesources.html">Manage capture sources</a></li>
+        ${
+          this.user?.role === 'admin'
+            ? html`
+          <li><a href="/capturesources.html">Manage capture sources</a></li>   
+        `
+            : html``
+        }
+        |
+        ${user}
     </menu>
     `
   }
@@ -52,6 +76,14 @@ export class SiteNav extends LitElement {
   a:hover {
     color: var(--bright-blue-500);
     text-decoration: underline;
+  }
+  .tag {
+      text-transform: uppercase;
+      background-color: var(--blue-300);
+      color: var(--blue-900);
+      font-size: 0.7rem;
+      border-radius: 4px;
+      padding: 0.25em 0.5em;
   }
   `
 }
