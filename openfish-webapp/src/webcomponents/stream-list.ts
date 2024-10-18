a92479bb8f30318f68d0ce4ee34917dec6424622
Scott Barnard <scott@ausocean.org>
2024-10-18 14:17:23 +1100

    Add new UI for viewing streams with video thumbnails

--- openfish-webapp/src/webcomponents/stream-list.ts
+++ openfish-webapp/src/webcomponents/stream-list.ts
@@ -1,10 +1,16 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property, state } from 'lit/decorators.js'
-import type { Result, VideoStream } from '../utils/api.types.ts'
+import type { CaptureSource, Result, VideoStream } from '../utils/api.types.ts'
 import { repeat } from 'lit/directives/repeat.js'
 import resetcss from '../styles/reset.css'
-import { datetimeDifference, formatAsDatetime, formatDuration } from '../utils/datetime.ts'
+import { datetimeDifference, formatAsDate, formatDuration } from '../utils/datetime.ts'
 import type { Filter } from './stream-filter.ts'
+import { extractVideoID } from '../utils/youtube.ts'
+
+type VideoStreamItem = Omit<VideoStream, 'capturesource'> & {
+  capturesource: CaptureSource
+  first?: boolean
+}
 
 @customElement('stream-list')
 export class StreamList extends LitElement {
@@ -18,7 +24,7 @@ export class StreamList extends LitElement {
   protected _page = 1
 
   @state()
-  protected _items: VideoStream[] = []
+  protected _items: VideoStreamItem[] = []
 
   @state()
   protected _totalPages = 0
@@ -32,20 +38,45 @@ export class StreamList extends LitElement {
   }
 
   async fetchData() {
-    const perPage = 10
+    // First page has 12 items, the rest have 15 because of this particular layout.
+    const perPage = 15
+    const perPageFirst = 12
 
     try {
       const params = new URLSearchParams()
-      params.set('limit', String(10))
-      params.set('offset', String((this._page - 1) * perPage))
+      if (this._page === 1) {
+        params.set('limit', String(perPageFirst))
+        params.set('offset', String(0))
+      } else {
+        params.set('limit', String(perPage))
+        params.set('offset', String(perPageFirst + (this._page - 2) * perPage))
+      }
 
       for (const key in this._filter) {
         params.set(key, String(this._filter[key as keyof Filter]))
       }
 
+      // Get videostreams and join on their capture source.
       const res = await fetch(`/api/v1/videostreams?${params.toString()}`)
       const data = (await res.json()) as Result<VideoStream>
-      this._items = data.results
+
+      const promises: Promise<VideoStreamItem>[] = []
+      for (const stream of data.results) {
+        promises.push(
+          (async (stream: VideoStream) => {
+            const res = await fetch(`/api/v1/capturesources/${stream.capturesource}`)
+            return {
+              ...stream,
+              capturesource: await res.json(),
+            }
+          })(stream)
+        )
+      }
+      const items = await Promise.all(promises)
+      if (this._page === 1 && items.length > 0) {
+        items[0].first = true
+      }
+      this._items = items
       this._totalPages = Math.floor(data.total / perPage)
     } catch (error) {
       console.error(error)
@@ -63,20 +94,17 @@ export class StreamList extends LitElement {
   }
 
   render() {
-    const header = html`
-    <tr>
-      <th>Stream URL</th>
-      <th>Date & Time</th>
-      <th>Duration</th>
-    </tr>
-    `
-
-    const rows = (stream: VideoStream) => html`
-    <tr onclick="window.location = '/watch.html?id=${stream.id}'">
-      <td>${stream.stream_url}</td>
-      <td>${formatAsDatetime(stream.startTime)}</td>
-      <td>${formatDuration(datetimeDifference(stream.endTime, stream.startTime))}</td>
-    </tr>
+    const streams = (stream: VideoStreamItem) => html`
+    <article onclick="window.location = '/watch.html?id=${stream.id}'" class="${stream.first ? 'first-item' : ''}">
+      <div class="img-container">
+        <img src="https://i.ytimg.com/vi/${extractVideoID(stream.stream_url)}/maxresdefault.jpg">
+        <span class="duration">${formatDuration(datetimeDifference(stream.endTime, stream.startTime))}</span>
+      </div>
+      <footer>
+        <h3>${stream.capturesource.name}</h3>
+        <p>${formatAsDate(stream.startTime)}</p>
+      </footer>
+    </article>
     `
 
     const pagination = html`   
@@ -86,68 +114,93 @@ export class StreamList extends LitElement {
     `
 
     return html`
-    <table>
-    <thead>
-      ${header}
-    </thead>
-    <tbody>
-      ${repeat(this._items, rows)}
-    </tbody>
-    <tfoot>
+    <main>
+      ${repeat(this._items, streams)}
+    </main>
+    <footer class="pagination">
       ${pagination}
-    </tfoot>
-    </table>
+    </footer>
     `
   }
 
   static styles = css`
     ${resetcss}
 
-    table {
-      display: grid;  
-      grid-template-columns: 1fr 20rem 10rem;
-      border-radius: 0.25rem;
-      border: 1px solid var(--gray-100);
+    main {
+      display: grid;
+      grid-template-columns: repeat(5, 1fr);
+      grid-template-rows: repeat(3, 1fr);
+      gap: 1rem;
     }
 
-    thead, tbody, tr {
-      display: contents;
+    .first-item {
+      grid-row: span 2;
+      grid-column: span 2;
+
+      footer {
+        padding: 1rem;
+      }
+      h3 {
+        font-size: 2rem;
+      }
     }
 
-    tbody td {
-      padding: 0.5rem 1rem;
-      border-bottom: 1px solid var(--gray-100);
-    }
-    tr {
-      cursor: pointer
-    }
-    tr:hover td {
-      background-color: var(--gray-50);
-      color: var(--blue-700)
+    article {
+      display: flex;
+      flex-direction: column;
+      cursor: pointer;
+      border-radius: 0.5rem;
+      background-color: var(--gray-100);
+      border: 1px solid var(--blue-400);
+      overflow: clip;
+      box-shadow: var(--shadow-soft-sm);
+      transition: box-shadow;
+      transition-duration: 200ms;
+
+      &:hover {
+        box-shadow: var(--shadow-soft-md);
+      }
+      
+      footer {
+        padding: 0.5rem;
+        font-size: 0.8rem;
+        display: flex;
+        flex-wrap: wrap;
+
+        * {
+          width: 100%;
+        }
+      }
+      img {
+        display: block;
+        width: 100%;
+      }
+
+      .img-container {
+        position: relative;
+      }
+
+      .duration {
+          position: absolute;
+          bottom: 0.5em;
+          right: 0.5em;
+
+          font-size: 0.8rem;
+          background-color: rgba(0, 0, 0, 0.8);
+          color: var(--bg);
+          padding: 0 .5rem;
+          border-radius: 999999px;
+          white-space: pre-line;
+      }
     }
 
-    thead th {
-      background-color: var(--gray-50);
-      border-bottom: 1px solid var(--gray-100);
-      padding: 0.5em 0;
-    }
-
-     tfoot {
-      background-color: var(--gray-50);
-      padding: 0.5em 0;
-      grid-column: 1/ span 3;
+    footer.pagination {
       display: flex;
       justify-content: center;
-      gap: 0.25rem
+      gap: 0.25rem;
+      padding: 1rem 0;
     }
-
-    th {
-      padding: 0.5rem
-    }
-    
-    .mr-1 {
-      margin-right: 1rem;
-    }`
+    `
 }
 
 declare global {
