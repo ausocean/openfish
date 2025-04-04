32ab07b3d9a17bd0119d1073bcd712f23a4dcf1c
Scott Barnard <scott@ausocean.org>
2025-04-04 16:31:36 +1100

    Add <image-carousel> component for our species pictures.
    Works with both mouse (clicking) and touch (swiping)
    
    Usage:
    ```html
    <image-carousel class="w-80 h-80 block">
        <img src="https://placehold.co/400">
        <img src="https://placehold.co/400">
        <img src="https://placehold.co/400">
    </image-carousel>
    ```

---
+++ site/src/webcomponents/image-carousel.ts
@@ -0,0 +1,69 @@
+import { TailwindElement } from './tailwind-element'
+import { css, html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+import { unsafeSVG } from 'lit/directives/unsafe-svg.js'
+import caretLeft from '../icons/caret-left.svg?raw'
+import caretRight from '../icons/caret-right.svg?raw'
+
+@customElement('image-carousel')
+export class ImageCarousel extends TailwindElement {
+  private _idx = 0
+
+  _handleScrollend() {
+    console.log('handle scroll end')
+    // Get slotted elements.
+    const slot = this.shadowRoot?.querySelector('slot')
+    if (slot !== undefined && slot !== null) {
+      const slottedElems = slot.assignedElements({ flatten: true })
+
+      // Find element currently in view.
+      this._idx = slottedElems.findIndex((elem) => {
+        const rect = elem.getBoundingClientRect()
+        const parentRect = this.getBoundingClientRect()
+        return rect.left >= parentRect.left && rect.right <= parentRect.right
+      })
+    }
+  }
+
+  goto(direction: 1 | -1) {
+    // Get slotted elements.
+    const slot = this.shadowRoot?.querySelector('slot')!
+    const slottedElems = slot.assignedElements({ flatten: true })
+
+    // Scroll next or previous into view.
+    this._idx = (this._idx + direction + slottedElems.length) % slottedElems.length
+    slottedElems[this._idx].scrollIntoView({ behavior: 'smooth' })
+  }
+
+  render() {
+    return html`
+        <div class="relative group w-full h-full">
+            <div @scrollend=${this._handleScrollend} class="absolute inset-0 flex border border-blue-800 aspect-square w-full overflow-clip  overflow-x-scroll snap-x snap-mandatory snap-scroll-smooth">
+                <slot></slot>
+            </div>
+
+            <button class="absolute left-2 bottom-2 rounded-full btn variant-transparent p-0 group-hover:opacity-100 opacity-0 transition-all text-slate-50 aspect-square h-8 w-8 *:w-4 *:h-4 *:fill-current" @click=${() => this.goto(-1)}>
+                ${unsafeSVG(caretLeft)}
+            </button>
+            <button class="absolute right-2 bottom-2 rounded-full btn variant-transparent p-0 group-hover:opacity-100 opacity-0 transition-all text-slate-50 aspect-square h-8 w-8 *:w-4 *:h-4 *:fill-current" @click=${() => this.goto(+1)}>
+                ${unsafeSVG(caretRight)}
+            </button>
+        </div>
+    `
+  }
+
+  static styles = [
+    TailwindElement.styles!,
+    css`
+        ::slotted(*) {
+            scroll-snap-align: center;
+        }
+    `,
+  ]
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'image-carousel': ImageCarousel
+  }
+}
