cb9b60feaad2a1b2fb525bbd89ca848f7a0b8836
Scott Barnard <scott@ausocean.org>
2025-05-23 10:39:49 +1000

    Run code formatter on whole project

--- docs/developer-docs/api/tags/[name].paths.ts
+++ docs/developer-docs/api/tags/[name].paths.ts
@@ -2,12 +2,11 @@ import { useOpenapi } from 'vitepress-openapi/client'
 import spec from '@openfish/client/swagger.json' with { type: 'json' }
 
 export default {
-    paths() {
-        const openapi = useOpenapi({ spec })
+  paths() {
+    const openapi = useOpenapi({ spec })
 
-        const json = openapi.spec as typeof spec
+    const json = openapi.spec as typeof spec
 
-        return json.tags.map(tag => ({params: tag}))
-
-    },
+    return json.tags.map((tag) => ({ params: tag }))
+  },
 }
