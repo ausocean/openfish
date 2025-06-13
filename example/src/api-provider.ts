77c0c2edbf4a82138fba5938f772bbfc2f342969
Scott Barnard <scott@ausocean.org>
2025-06-13 10:18:12 +1000

    Remove example because it breaks CI

--- example/src/api-provider.ts
+++
@@ -1,6 +0,0 @@
-import { useApiProvider } from '@openfish/ui/components/api-provider.ts'
-import { createClient } from '@openfish/client'
-
-const client = createClient({ baseUrl: 'http://localhost:8080/' })
-
-useApiProvider(client)
--- example/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import config from '@openfish/ui/vite.config'
-import { mergeConfig } from 'vite'
-
-export default mergeConfig(config, {
-  root: '.',
-})
