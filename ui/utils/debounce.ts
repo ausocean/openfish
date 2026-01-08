72b9d29a22dd2a0f500a9b4b04860b4c0c2c7c68
Elliot Shine <elliot@ausocean.org>
2026-01-08 13:01:36 +1030

    ui: debounce species search This resolves a race condition that could cause duplicate results when typing too quickly. It also reduces the number of requests sent.

---
+++ ui/utils/debounce.ts
@@ -0,0 +1,16 @@
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+
+  // Use ReturnType<typeof setTimeout> to handle both Node and Browser environments.
+  let timer: ReturnType<typeof setTimeout>;
+
+  return function (this: any, ...args: Parameters<T>) {
+    const context = this;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
